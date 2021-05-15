a17414d3660cf9015aea2e9f95bae7849c5683aa
macki-int <macki@int.pl>
2021-05-15 23:16:27 +0200

    add main menu

--- src/main/java/pl/mj/olaf/menu/MainMenu.java
+++ src/main/java/pl/mj/olaf/menu/MainMenu.java
@@ -1,4 +1,13 @@
 package pl.mj.olaf.menu;
 
 public class MainMenu {
+
+    public void showMainMenu(){
+        System.out.println("----------OLAF------------");
+        System.out.println("1. Lista studentów");
+        System.out.println("2. Nowy student");
+        System.out.println("0. Wyloguj");
+
+
+    }
 }
