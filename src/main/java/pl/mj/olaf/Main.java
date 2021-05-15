3dc126ffd61a63bfa6aefdb351a0d5a9613e2d77
macki-int <macki@int.pl>
2021-05-15 23:32:04 +0200

    add call showLoginMenu method

--- src/main/java/pl/mj/olaf/Main.java
+++ src/main/java/pl/mj/olaf/Main.java
@@ -1,7 +1,12 @@
 package pl.mj.olaf;
 
+import pl.mj.olaf.menu.LoginMenu;
+
 public class Main {
     public static void main(String[] args) {
+        LoginMenu loginMenu = new LoginMenu();
+        loginMenu.showLoginMenu();
+        loginMenu.getMenuChoice();
 
     }
 }
