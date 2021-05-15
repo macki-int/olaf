401aa669562076aa55531950cecda01085d89ab1
macki-int <macki@int.pl>
2021-05-15 23:18:45 +0200

    add login menu

---
+++ src/main/java/pl/mj/olaf/menu/LoginMenu.java
@@ -0,0 +1,9 @@
+package pl.mj.olaf.menu;
+
+public class LoginMenu {
+    public void showLoginMenu(){
+        System.out.println("----------OLAF------------");
+        System.out.println("1. Logowanie");
+        System.out.println("0. Zakończ program");
+    }
+}
