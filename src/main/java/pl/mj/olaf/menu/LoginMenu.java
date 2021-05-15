51683aa307ef0211449e29e1c60c132027c38c45
macki-int <macki@int.pl>
2021-05-15 23:30:44 +0200

    add getMenuChoice method

--- src/main/java/pl/mj/olaf/menu/LoginMenu.java
+++ src/main/java/pl/mj/olaf/menu/LoginMenu.java
@@ -1,9 +1,21 @@
 package pl.mj.olaf.menu;
 
+import java.util.Scanner;
+
 public class LoginMenu {
+
     public void showLoginMenu(){
         System.out.println("----------OLAF------------");
         System.out.println("1. Logowanie");
         System.out.println("0. Zakończ program");
+        System.out.println("--------------------------");
+        System.out.println("Wybierz pozycję menu:");
+    }
+
+    public void getMenuChoice(){
+        Scanner scanner = new Scanner(System.in);
+        int menuChoice = scanner.nextInt();
+        System.out.println(menuChoice);
+
     }
 }
