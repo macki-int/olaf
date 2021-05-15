dbf5eaf40a53602d81e8aa970e17faea2fe4ee1d
macki-int <macki@int.pl>
2021-05-15 23:42:35 +0200

    add login variable with info about choice

--- src/main/java/pl/mj/olaf/Main.java
+++ src/main/java/pl/mj/olaf/Main.java
@@ -3,10 +3,15 @@ package pl.mj.olaf;
 import pl.mj.olaf.menu.LoginMenu;
 
 public class Main {
+
     public static void main(String[] args) {
         LoginMenu loginMenu = new LoginMenu();
+
         loginMenu.showLoginMenu();
-        loginMenu.getMenuChoice();
+        boolean login = loginMenu.getMenuChoice();
+        if (login){
+            System.out.println("login");
+        }
 
     }
 }
