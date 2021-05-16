61a97ac99abc417a54e6578ca9d8de8a65bb24e0
macki-int <macki@int.pl>
2021-05-16 14:30:00 +0200

    change type of scanner variable on Character

--- src/main/java/pl/mj/olaf/menu/LoginMenu.java
+++ src/main/java/pl/mj/olaf/menu/LoginMenu.java
@@ -7,18 +7,18 @@ public class LoginMenu {
     public void showLoginMenu() {
         System.out.println("----------OLAF------------");
         System.out.println("1. Logowanie");
-        System.out.println("0. Zakończ program");
+        System.out.println("X. Zakończ program");
         System.out.println("--------------------------");
         System.out.println("Wybierz pozycję menu:");
     }
 
     public boolean getMenuChoice() {
         Scanner scanner = new Scanner(System.in);
-        int menuChoice = scanner.nextInt();
+        Character menuChoice = scanner.next().charAt(0);
         //TODO validate choice
         //TODO try-catch
 
-        if (menuChoice == 1) {
+        if (menuChoice.equals("1")) {
             return true;
         }
         return false;
