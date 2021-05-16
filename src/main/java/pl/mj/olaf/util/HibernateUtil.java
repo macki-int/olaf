4add3fa11acc75553767aea8a1acaceb12aabbb3
macki-int <macki@int.pl>
2021-05-17 00:29:13 +0200

    remove comments line

--- src/main/java/pl/mj/olaf/util/HibernateUtil.java
+++ src/main/java/pl/mj/olaf/util/HibernateUtil.java
@@ -15,21 +15,6 @@ public class HibernateUtil {
             try {
                 Configuration configuration = new Configuration();
 
-//                Properties settings = new Properties();
-//                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-//                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/hibernate_db?useSSL=false");
-//                settings.put(Environment.USER, "root");
-//                settings.put(Environment.PASS, "root");
-//                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-//
-//                settings.put(Environment.SHOW_SQL, "true");
-//
-//                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-//
-//                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
-//
-//                configuration.setProperties(settings);
-
                 configuration.addAnnotatedClass(Student.class);
 
                 ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
