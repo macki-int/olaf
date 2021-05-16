c43986b705708ceed6ada98ac1ad17190190f9ec
macki-int <macki@int.pl>
2021-05-17 00:28:08 +0200

    test save to db

--- src/main/java/pl/mj/olaf/Main.java
+++ src/main/java/pl/mj/olaf/Main.java
@@ -1,11 +1,29 @@
 package pl.mj.olaf;
 
+import org.hibernate.Session;
+import pl.mj.olaf.dao.StudentDao;
 import pl.mj.olaf.menu.LoginMenu;
+import pl.mj.olaf.model.Student;
+import pl.mj.olaf.util.HibernateUtil;
+
+import java.time.LocalDate;
 
 public class Main {
 
     public static void main(String[] args) {
         LoginMenu loginMenu = new LoginMenu();
+
+        StudentDao studentDao = new StudentDao();
+
+//        Student student = new Student("Na", "Ra", "ram.pa@ta.com", LocalDate.of(1999,1,1));
+
+
+//        studentDao.saveStudent(student);
+        for (Student student : studentDao.getStudents()) {
+            System.out.println(student.getId());
+        }
+
+
 
         loginMenu.showLoginMenu();
         boolean login = loginMenu.getMenuChoice();
