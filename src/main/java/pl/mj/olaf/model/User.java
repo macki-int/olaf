ca7570316bc33ef6e90c59ec3ca8fd2bcba26d66
macki-int <macki@int.pl>
2021-05-16 18:57:45 +0200

    add column names for fields

--- src/main/java/pl/mj/olaf/model/User.java
+++ src/main/java/pl/mj/olaf/model/User.java
@@ -1,15 +1,29 @@
 package pl.mj.olaf.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.UUID;
 
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @Column(name = "id")
     private UUID id;
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "e_mail")
     private String eMail;
     private String password;
 
-    public User(UUID id, String firstName, String lastName, String eMail, String password) {
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String eMail, String password) {
         this.id = UUID.randomUUID();
         this.firstName = firstName;
         this.lastName = lastName;
